7b4de81cb1be228be260c97936f25c126f4c3a16
zhangjinke <zhangjinke888@163.com>
2021-09-29 11:07:20 +0800

    Springcloud

---
+++ springcloud/cloudalibaba-sentinel-service8401/src/main/java/com/keer/springcloud/MainApp8401.java
@@ -0,0 +1,18 @@
+package com.keer.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @auther keer
+ * @data 2021/9/28
+ * @description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ springcloud/cloudalibaba-sentinel-service8401/src/main/java/com/keer/springcloud/controller/SentinelController.java
@@ -0,0 +1,28 @@
+package com.keer.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auther keer
+ * @data 2021/9/28
+ * @description
+ */
+@RestController
+@Slf4j
+public class SentinelController {
+    @GetMapping("/testA")
+    public String testA()
+    {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB()
+    {
+        log.info(Thread.currentThread().getName()+"\t"+"...testB");
+        return "------testB";
+    }
+
+}
